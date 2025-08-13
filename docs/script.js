7c03243f15b8483ad668ea4cfa5a771004ac77b5
Deepak-rajv <deepak3622a@gmail.com>
2025-08-13 12:48:33 +0530

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,7 @@
+function bulboff(){
+    document.getElementById("image").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkuTs3yIlg7XZIPbxYvkrLCtm82AZBjFGx6ESdopd6y7NXpnhMjGs1Fi6P-mxt47_5ZBk&usqp=CAU";
+
+}
+function bulbon(){
+    document.getElementById("image").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThxsY66h305izj0unRBW9tz88GmAcw_VtA88SNOdMuiy91gMh-qXbAgMzrB81snxeL6hQ&usqp=CAU";
+}
